8fe417f4a4a295ea573f1781f057cf9671ed4082
Jeremy <jeremyle420@gmail.com>
2023-08-22 22:51:27 +1000

    finished prototype version 1

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # sleep-bot
+
 A discord bot which pings online users and tells them to go to sleep.
+
+## Set up
+
+Add your discord bot's token in a file called `config.json` like so:
+
+```json
+{
+  "token": "your-token-goes-here"
+}
+```
+
+Make sure you have a text channel called `sleep`.
+
+Run `node index.js` to start up the bot.
--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, Intents } = require("discord.js");
 const { token } = require("./config.json");
 
+const { phrases } = require("./phrases.json");
+
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildPresences,
+  ],
+});
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
+  console.log("Ready!");
+
+  client.guilds.cache.forEach((g) => {
+    const awake = [];
+    g.presences.cache.forEach((m) => {
+      if (m.userId === "1143401901027754004") return;
+      const userId = m.userId;
+      const user = g.members.cache.get(userId);
+      const name = user.nickname || user.user.globalName || user.user.username;
+
+      const activities = m.activities.map((a) => a.name);
+      let message = activities.sort().join(" + ");
+      message = message ? `: Playing ${message}` : "";
+
+      awake.push(`${name} <@${userId}>${message}`.trim());
+    });
+
+    const channel = g.channels.cache.find((p) => p.name === "sleep");
+    if (awake.length) {
+      const line = phrases[parseInt(Math.random() * phrases.length)];
+
+      awake.sort();
+      const res = [
+        "# SLEEP",
+        line,
+        "",
+        ...awake.map((a) => a.split(" ").slice(1).join(" ")),
+      ];
+      channel.send(res.join("\n"));
+    } else {
+      channel.send("Congrats! 🥳 Everyone slept on time!");
+    }
+  });
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.member.user.bot) return;
+
+  message.react("😴");
+  message.reply("Go to Sleep Please! 🥺");
 });
 
 // Log in to Discord with your client's token
